6e13c6dbec13b597111cb73c3e606640f945d283
Sid Senthil <sidsenthil2010@gmail.com>
2025-08-08 20:50:56 +0530

    Continue working on gamedata

---
+++ test/java/game/PlayGame.java
@@ -0,0 +1,11 @@
+package game;
+
+import game.Constants.Move;
+import game.Constants.PlayerType;
+import game.Constants.PlayerID;
+
+public class PlayGame {
+
+
+
+}
--- test/java/game/objects/GameData.java
+++ test/java/game/objects/GameData.java
@@ -9,17 +9,17 @@ import game.Constants.Points;
 public class GameData {
     private int round;
 
-    private ArrayList<Move> player1Moves;
-    private ArrayList<Move> player2Moves;
+    private final ArrayList<Move> player1Moves;
+    private final ArrayList<Move> player2Moves;
 
-    private ArrayList<Integer> player1Points;
-    private ArrayList<Integer> player2Points;
+    private final ArrayList<Integer> player1Points;
+    private final ArrayList<Integer> player2Points;
 
-    private PlayerType player1Type;
-    private PlayerType player2Type;
+    private final PlayerType player1Type;
+    private final PlayerType player2Type;
 
-    private PlayerID player1ID;
-    private PlayerID player2ID;
+    private final PlayerID player1ID;
+    private final PlayerID player2ID;
 
     public GameData(PlayerType player1Type, PlayerType player2Type, PlayerID player1ID, PlayerID player2ID) {
         this.player1Type = player1Type;
@@ -52,7 +52,63 @@ public class GameData {
             this.player1Points.add(Points.TIE_BOTH_DEFECT);
             this.player2Points.add(Points.TIE_BOTH_DEFECT);
         }
+    }
 
+    public int getLatestRoundNumber() {
+        return this.player1Moves.size();
+    }
+
+    public int getPlayer1Points() {
+        int sum = 0;
+        for (int point : player1Points) {
+            sum += point;
+        }
+        return sum;
+    }
+
+    public int getPlayer2Points() {
+        int sum = 0;
+        for (int point : player2Points) {
+            sum += point;
+        }
+        return sum;
+    }
+
+    public Move getPlayer1Move(int index) {
+        return this.player1Moves.get(index);
+    }
+
+    public Move getPlayer2Move(int index) {
+        return this.player2Moves.get(index);
+    }
+
+    public PlayerType getPlayer1Type() {
+        return this.player1Type;
+    }
+
+    public PlayerType getPlayer2Type() {
+        return this.player2Type;
+    }
+
+    public PlayerID getPlayer1ID() {
+        return this.player1ID;
+    }
+
+    public PlayerID getPlayer2ID() {
+        return this.player2ID;
+    }
+
+    public int getWinner() {
+        int player1Points = getPlayer1Points();
+        int player2Points = getPlayer2Points();
+
+        if (player1Points > player2Points) {
+            return 1;
+        } else if (player1Points < player2Points) {
+            return 2;
+        } else {
+            return 0;
+        }
     }
 
 
